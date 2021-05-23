8918b9a82a8a7d4c2dba565837580b246b8ae3e6
Siyovush Kadyrov <siyovush.kadyrov@gmail.com>
2021-05-23 10:33:27 +0300

    Image container implemented

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
 import unsplashApiKey from './keys/keys.js';
-console.log(unsplashApiKey);
+
+
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
 // Setup unsplash
 const fetchedImageCount = 10;
 const unsplashApiUrl = `https://api.unsplash.com/photos/random/?client_id=${unsplashApiKey}&count=${fetchedImageCount}`;
 
-// Get photos
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+function displayPhotos() {
+    photosArray.forEach((photo) => {
+        const anchor = document.createElement('a');
+        setAttributes(anchor, {
+            href: photo.links.html,
+            target: '_blank'
+        });
+
+        const img = document.createElement('img');
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        });
+
+        anchor.appendChild(img);
+        imageContainer.appendChild(anchor);
+    });
+}
+
 async function getPhotos() {
     try {
         const response = await fetch(unsplashApiUrl);
-        const date = await response.json();
-        console.log(data);
+        photosArray = await response.json();
+        displayPhotos();
     } catch (error) {
         
     }
